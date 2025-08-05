b1ac5a626081bd16d32b7c9467cad24a7195998c
kathanxx <lalpurwalakathan@gmail.com>
2025-08-05 11:23:42 +1000

    initial commit - kay

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
 
         System.out.println("Josh Commit!");
         System.out.println("Kathan Commit!");
-        System.out.println("test");
+        System.out.println("tested");
         }
     }
