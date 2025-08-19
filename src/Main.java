2e33b5d5cbedad624748bf698f5e8d5304ec238d
Ali Attaee <n11602309@qut.edu.au>
2025-08-19 10:23:44 +1000

    init commit - Ali Sina

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,7 @@ public class Main {
         System.out.println("Josh Commit!");
         System.out.println("Kathan Commit!");
         System.out.println("Noah Commit Test");
-        System.out.println("Ali Commit!");
+        System.out.println("Ali Sina Commit!");
         System.out.println("Quant commit");
 	System.out.println("Bilal commit");
 
