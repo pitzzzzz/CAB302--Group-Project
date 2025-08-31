b4cbf6c905fff682a6c14f62794347dc47907a0d
Pitzzz <jpapitz@gmail.com>
2025-08-31 20:52:41 +1000

    update: refactored codebase for javafx

---
+++ src/main/java/module-info.java
@@ -0,0 +1,11 @@
+module com.javaninjas.careerpathway {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    // add in the other project modules in here when trying to run the application
+    exports com.javaninjas.careerpathway.app;
+    opens com.javaninjas.careerpathway.app to javafx.fxml;
+
+    exports com.javaninjas.careerpathway.controllers;
+    opens com.javaninjas.careerpathway.controllers to javafx.fxml;
+}
