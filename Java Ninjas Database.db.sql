836b63920c19e9fd64e17bb214c6c910657d39f3
b1 <bilalabdul20059@gmail.com>
2025-08-31 22:10:11 +1000

    added first and last name to databse sql file - Bilal(bill)

--- Java Ninjas Database.db.sql
+++ Java Ninjas Database.db.sql
@@ -1,24 +1,26 @@
 BEGIN TRANSACTION;
+
 CREATE TABLE IF NOT EXISTS "Courses" (
-	"CourseID"	INTEGER,
-	"Code"	TEXT UNIQUE,
-	"Awards"	TEXT CHECK("Awards" IN ('Diploma', 'Bachelor', 'Honours', 'Graduate Certificate', 'Graduate Diploma', 'Master')),
-	"DurationYears"	REAL CHECK("DurationYears" > 0),
-	"StudyChoice"	TEXT CHECK("StudyChoice" IN ('On-Campus', 'Online')),
-	"Field"	TEXT,
-	"CreatedAt"	datetime DEFAULT CURRENT_TIMESTAMP,
-	PRIMARY KEY("CourseID" AUTOINCREMENT)
+    "CourseID"       INTEGER PRIMARY KEY AUTOINCREMENT,
+    "Code"           TEXT UNIQUE,
+    "Awards"         TEXT CHECK("Awards" IN ('Diploma', 'Bachelor', 'Honours', 'Graduate Certificate', 'Graduate Diploma', 'Master')),
+    "DurationYears"  REAL CHECK("DurationYears" > 0),
+    "StudyChoice"    TEXT CHECK("StudyChoice" IN ('On-Campus', 'Online')),
+    "Field"          TEXT,
+    "CreatedAt"      DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
 CREATE TABLE IF NOT EXISTS "User" (
-	"UserID"	INTEGER,
-	"Email"	TEXT NOT NULL UNIQUE,
-	"HasedPassword"	TEXT NOT NULL,
-	"Age"	INTEGER NOT NULL CHECK("Age" >= 13 AND "Age" <= 120),
-	"Profile Role"	TEXT NOT NULL,
-	"YearLevel"	TEXT,
-	"CreatedAt"	datetime DEFAULT CURRENT_TIMESTAMP,
-	"TargetIntakeYear"	INTEGER CHECK("TargetIntakeYear" BETWEEN 2025 AND 2040),
-	PRIMARY KEY("UserID" AUTOINCREMENT),
-	CHECK("YearLevel" IN ('Year 10', 'Year 11', 'Year 12', 'Gap Year', 'University', 'Graduate', 'Other'))
+    "UserID"           INTEGER PRIMARY KEY AUTOINCREMENT,
+    "FirstName"        TEXT NOT NULL,
+    "LastName"         TEXT NOT NULL,
+    "Email"            TEXT NOT NULL UNIQUE,
+    "HashedPassword"   TEXT NOT NULL,
+    "Age"              INTEGER NOT NULL CHECK("Age" >= 13 AND "Age" <= 120),
+    "ProfileRole"      TEXT NOT NULL,
+    "YearLevel"        TEXT CHECK("YearLevel" IN ('Year 10', 'Year 11', 'Year 12', 'Gap Year', 'University', 'Graduate', 'Other')),
+    "TargetIntakeYear" INTEGER CHECK("TargetIntakeYear" BETWEEN 2025 AND 2040),
+    "CreatedAt"        DATETIME DEFAULT CURRENT_TIMESTAMP
 );
+
 COMMIT;
